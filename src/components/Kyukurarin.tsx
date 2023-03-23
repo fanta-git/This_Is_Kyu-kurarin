c82ba7a464be5658e9a81dc4b20c355b72cefa18
fanta-git <fantaji4957t@gmail.com>
2023-03-23 16:55:13 +0900

    追加 きゅうくらりんできるように

--- src/components/Kyukurarin.tsx
+++ src/components/Kyukurarin.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useRef } from "react";
-import { BACKGROUND, HEIGHT, WIDTH } from "../const/canvas";
+import { CSSProperties, useEffect, useRef } from "react";
+import { BACKGROUND, HEIGHT, SPACING, WIDTH } from "../const/canvas";
 
 type Props = {
   fileUrl: string | undefined
+}
+
+const style: CSSProperties = {
+  width: "100%",
+  aspectRatio: "16 / 9"
 }
 
 const Kyukurarin = (props: Props) => {
@@ -12,18 +17,24 @@ const Kyukurarin = (props: Props) => {
   useEffect(() => {
     const ctx = canvasRef.current?.getContext('2d');
     if (ctx == null) return;
+
     ctx.fillStyle = BACKGROUND;
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
     if (fileUrl === undefined) return;
     const img = new Image();
     img.src = fileUrl;
     img.onload = () => {
-      ctx.drawImage(img, 0, 0);
+      const scale = HEIGHT / img.height;
+      const costomWidth = img.height * scale | 0;
+      const centerLeft = (WIDTH - costomWidth) / 2 | 0;
+
+      for (let i = 10; i >= 0; i--) ctx.drawImage(img, centerLeft - SPACING * i, 0, HEIGHT, costomWidth);
     };
   }, [fileUrl]);
 
   return (
-    <canvas width={WIDTH} height={HEIGHT} style={{ width: "100%", aspectRatio: "16 / 9" }} ref={canvasRef} />
+    <canvas width={WIDTH} height={HEIGHT} style={style} ref={canvasRef} />
   );
 };
 
--- src/const/canvas.ts
+++ src/const/canvas.ts
@@ -1,3 +1,4 @@
 export const WIDTH = 1280;
 export const HEIGHT = 720;
 export const BACKGROUND = "#F7D1DC";
+export const SPACING = 66;
