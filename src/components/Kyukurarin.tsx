c37c0ed9202b548ff27a8cd14e0838a0867825f7
fanta-git <fantaji4957t@gmail.com>
2023-03-23 15:04:44 +0900

    追加 背景色

--- src/components/Kyukurarin.tsx
+++ src/components/Kyukurarin.tsx
@@ -9,17 +9,21 @@ const Kyukurarin = (props: Props) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const context = canvasRef.current?.getContext('2d');
-    if (context == null || fileUrl === undefined) return;
+    const canvas = canvasRef.current;
+    if (canvas == null) return;
+    const ctx = canvas?.getContext('2d')!;
+    ctx.fillStyle = "#F7D1DC";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (ctx == null || fileUrl === undefined) return;
     const img = new Image();
     img.src = fileUrl;
     img.onload = () => {
-      context.drawImage(img, 0, 0);
+      ctx.drawImage(img, 0, 0);
     };
   }, [fileUrl]);
 
   return (
-    <canvas width="1280" height="720" ref={canvasRef} />
+    <canvas width="1280" height="720" style={{ width: "100%" }} ref={canvasRef} />
   );
 };
 
