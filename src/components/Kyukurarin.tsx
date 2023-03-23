31ab4272eafae44f724b7533977943671ccf9e26
fanta-git <fantaji4957t@gmail.com>
2023-03-23 12:22:25 +0900

    追加 ドロップした画像の表示

--- src/components/Kyukurarin.tsx
+++ src/components/Kyukurarin.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useRef } from "react";
+
 type Props = {
   fileUrl: string | undefined
 }
 
 const Kyukurarin = (props: Props) => {
   const { fileUrl } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const context = canvasRef.current?.getContext('2d');
+    if (context == null || fileUrl === undefined) return;
+    const img = new Image();
+    img.src = fileUrl;
+    img.onload = () => {
+      context.drawImage(img, 0, 0);
+    };
+  }, [fileUrl]);
 
   return (
-    <>
-      {fileUrl}
-    </>
+    <canvas width="1280" height="720" ref={canvasRef} />
   );
 };
 
