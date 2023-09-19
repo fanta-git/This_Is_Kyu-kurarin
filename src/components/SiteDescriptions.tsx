0219ebc6a5c8e75fd8608beb35e88acccadd74e7
fanta-git <fantaji4957t@gmail.com>
2023-09-19 13:49:39 +0900

    追加 使い方ツイートの埋め込み

--- src/components/SiteDescriptions.tsx
+++ src/components/SiteDescriptions.tsx
@@ -1,4 +1,5 @@
-import { Heading, ListItem, Text, UnorderedList, Divider } from "@chakra-ui/react";
+import { Box, Heading, ListItem, Text, UnorderedList, Divider } from "@chakra-ui/react";
+import { Tweet } from 'react-twitter-widgets';
 import NicovideoPlayer from "./NicovideoPlayer";
 
 export default function SiteDescriptions () {
@@ -7,10 +8,13 @@ export default function SiteDescriptions () {
       <Text>
         上にある枠をクリックして画像を選択、または枠にドラッグ＆ドロップするときゅうくらりんのサムネみたいになります。
       </Text>
+      <Box w={"100%"}>
+        <Tweet tweetId="1638910366435672064" />
+      </Box>
       <Divider />
       <Heading as='h4' size='md'>注意点</Heading>
       <UnorderedList>
-        <ListItem>iOSに搭載されている被写体切り抜き機能を使って切り抜き、サイトにドラッグ&ドロップする使用法を想定しています。</ListItem>
+        <ListItem>iOSに搭載されている被写体切り抜き機能を使って切り抜き、サイトにドラッグ&ドロップする使用法を想定しています。Android等の場合、事前に画像編集ソフトなどで背景透過をする必要があります。</ListItem>
         <ListItem>選択する画像は背景が透過されていないと上手いこと行きません。</ListItem>
         <ListItem>横長の画像だったり、画質の低い画像だと綺麗に作れません。</ListItem>
       </UnorderedList>
