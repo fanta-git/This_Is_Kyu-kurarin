c31e33562de2cb137f65203b743aa853c975c52f
fanta-git <fantaji4957t@gmail.com>
2023-09-19 13:38:27 +0900

    変更 動画を埋め込みプレーヤーで再生できるように

--- src/components/SiteDescriptions.tsx
+++ src/components/SiteDescriptions.tsx
@@ -1,4 +1,5 @@
-import { Link, UnorderedList, ListItem, Text, Heading } from "@chakra-ui/react";
+import { Heading, ListItem, Text, UnorderedList, Divider } from "@chakra-ui/react";
+import NicovideoPlayer from "./NicovideoPlayer";
 
 export default function SiteDescriptions () {
   return (
@@ -6,16 +7,20 @@ export default function SiteDescriptions () {
       <Text>
         上にある枠をクリックして画像を選択、または枠にドラッグ＆ドロップするときゅうくらりんのサムネみたいになります。
       </Text>
+      <Divider />
       <Heading as='h4' size='md'>注意点</Heading>
       <UnorderedList>
         <ListItem>iOSに搭載されている被写体切り抜き機能を使って切り抜き、サイトにドラッグ&ドロップする使用法を想定しています。</ListItem>
         <ListItem>選択する画像は背景が透過されていないと上手いこと行きません。</ListItem>
         <ListItem>横長の画像だったり、画質の低い画像だと綺麗に作れません。</ListItem>
       </UnorderedList>
+      <Divider />
       <Heading as='h4' size='md'>元ネタ</Heading>
-      <Text>
-        <Link href="https://www.nicovideo.jp/watch/sm39257413">きゅうくらりん / いよわ feat.可不</Link>
-      </Text>
+      <NicovideoPlayer
+        id="sm39257413"
+        width={640}
+        height={360}
+      />
     </>
   );
 };
